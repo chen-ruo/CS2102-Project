a282facddb4cde2297377d717f91aafc83550a8d
Chen Ruo <chen.ruo@hotmail.com>
2015-10-18 16:50:47 +0800

    Employer Register
    Employer is able to register the company on the page and the data will
    be inserted into our database.

---
+++ employerRegister.php
@@ -0,0 +1,334 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="icon" href="http://i376.photobucket.com/albums/oo207/happyice/favicon-32x32_zpsci38sgpc.png">
+<?php
+putenv('ORACLE_HOME=/oraclient');
+$dbh = ocilogon('A0101002', 'crse1510', ' (DESCRIPTION =
+	(ADDRESS_LIST =
+	(ADDRESS = (PROTOCOL = TCP)(HOST = sid3.comp.nus.edu.sg)(PORT = 1521))
+	)
+	(CONNECT_DATA =
+	(SERVICE_NAME = sid3.comp.nus.edu.sg)
+	)
+	)');
+?>
+<title>One stop Job Portal</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="One stop job portal" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+
+              function checkServer(){
+                var username = document.getElementById("email").value
+                var password = document.getElementById("password").value
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				var companyname = document.getElementById("companyname").value
+				var companynum = document.getElementById("companynum").value
+				var companyurl = document.getElementById("companyurl").value
+				var industry = document.getElementById("industry").value
+				var addressofcompany = document.getElementById("addressofcompany").value
+				var natureofbusiness = document.getElementById("natureofbusiness").value
+				var postalcode = document.getElementById("addressofcompany").value
+				
+			var r = confirm("Are you sure to register for an account?");
+			if (r == true) {
+
+			 var errormsg = "Below are the errors:\n\n";
+			   var errorlog = false;
+			   
+			    if( username.trim().length == 0) {
+					errorlog = true;
+					errormsg += "- Email field is empty.\n";
+				} else if (!username.match(re)){
+					errorlog = true;
+					errormsg += "- Email is invalid. Please enter a valid email so that we can send a confirmation email to you.\n";
+				}
+			   if(password.trim().length < 4){
+					errorlog = true;
+					errormsg += "- Please enter a valid password of atleast 4 characters long.\n";
+				}
+			   if( companyname.trim().length == 0){
+					errorlog = true;
+					errormsg += "- Name of company is empty\n";
+				} if (companynum.trim().length == 0){
+					errorlog = true;
+					errormsg += "- company number is empty\n";
+					} if (addressofcompany.trim().length == 0){
+					errorlog = true;
+					errormsg += "- Missing address of company\n"
+					} if (industry.trim().length == 0){
+					errorlog = true;
+					errormsg += "- Industry is empty.\n"
+					}
+					if (natureofbusiness.trim().length == 0){
+					errorlog = true;
+					errormsg += "- Nature of business is empty.\n"
+					} 	if (postalcode.trim().length == 0){
+					errorlog = true;
+					errormsg += "- Company's postal code is empty.\n"
+					}
+	
+					if(errorlog){
+					errormsg += "\nPlease correct these errors before submitting.";
+					alert(errormsg);
+					} else{
+					setTimeout(setMain, 1000);
+				}
+			} else {
+			}
+              }
+			  
+			  function setMain(){
+				alert("Account created succcessfully");
+			  	window.location.href = "index.html";
+			}
+        </script>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""/></a>
+	    </div>
+                <!--/.navbar-header-->
+                <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
+	        <ul class="nav navbar-nav">
+			  <li><a href="about.html">About Us</a></li>
+		        <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jobs<b class="caret"></b></a>
+		            <ul class="dropdown-menu">
+			            <li><a href="jobs.html">Part-time Jobs</a></li>
+			            <li><a href="jobs.html">Internships</a></li>
+			            <li><a href="jobs.html">Full-time Jobs</a></li>
+		            </ul>
+		        </li>
+				        <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Employers<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="post.html">Post Jobs</a></li>
+						    <li><a href="search.html">Search applicants</a></li>
+							  <li><a href="searchmatched.html">Search for matched applicants</a></li>
+		             </ul>
+		        </li>
+				
+				<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerLogin.html">Employer login</a></li>
+						    <li><a href="applicantLogin.html">Applicant Login</a></li>
+		             </ul>
+		        </li>
+			<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Registration<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerRegister.html">Create an employer account</a></li>
+						    <li><a href="applicantRegister.html">Create an applicant account</a></li>
+		             </ul>
+		        </li>
+                <div class="clearfix"></div>
+            </div>
+	    <!--/.navbar-collapse-->
+	</nav>
+<div class="container">
+    <div class="single">  
+	   <div class="form-container">
+        <h2>Employer Register Form</h2>
+        <form>
+		
+		<!-- primary key -->
+		
+		  <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="email">Email</label>
+                <div class="col-md-9">
+                    <input type="text" name="email" path="email" id="email" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+		 
+			<div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="password">Password</label>
+                <div class="col-md-9">
+                    <input type="text" name="password" path="password" id="password" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+		 
+		 
+		 			<div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="companyname">Company name</label>
+                <div class="col-md-9">
+                    <input type="text" name="companyname"  path="companyname" id="companyname" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+
+		 
+		 <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="companynum">Company number</label>
+                <div class="col-md-9">
+                    <input type="text" name="companynum" path="companynum" id="companynum" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+		 
+		 		 			<div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="companyurl">Company URL</label>
+                <div class="col-md-9">
+                    <input type="text" name="companyurl" path="companyurl" id="companyurl" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+		 
+		 
+          <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="postalcode">Company Postal Code</label>
+                <div class="col-md-9">
+                    <input type="text" name="postalcode" path="postalcode" id="postalcode" class="form-control input-sm"/>
+                </div>
+            </div>
+         </div>
+		 
+         <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="natureofbusiness">Nature of Business</label>
+                <div class="col-md-9">
+                    <input type="text" name="natureofbusiness" path="natureofbusiness" id="natureofbusiness" class="form-control input-sm"/>
+                </div>
+            </div>
+        </div>
+		
+        <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="addressofcompany">Address of Company</label>
+                <div class="col-md-9">
+                    <input type="text" name="addressofcompany" path="addressofcompany" id="addressofcompany" class="form-control input-sm"/>
+                </div>
+            </div>
+        </div>
+		
+        <div class="row">
+            <div class="form-group col-md-12">
+                <label class="col-md-3 control-lable" for="industry">Industry</label>
+                <div class="col-md-9">
+                    <input type="text" name="industry" path="industry" id="industry" class="form-control input-sm"/>
+                </div>
+            </div>
+        </div>
+
+        <input type="submit" name="formSubmit" value="Submit" style="display: block; margin: 0 auto;" >
+
+		</form>
+
+		 <?php if(isset($_GET['formSubmit']))
+		{
+			$email = $_GET['email'];
+			$password = $_GET['password'];
+			$companyname = $_GET['companyname'];
+			$companynum = $_GET['companynum'];
+			$companyurl = $_GET['companyurl'];
+			$postalcode = $_GET['postalcode'];
+			$natureofbusiness = $_GET['natureofbusiness'];
+			$companyaddress = $_GET['addressofcompany'];
+			$industry = $_GET['industry'];
+
+			echo "$industry";
+
+
+			$sql = 'INSERT INTO employer (email, password, companyname, companynum, companyurl, postalcode, natureofbusiness, companyaddress, industry) VALUES(:email, :password, :companyname, :companynum, :companyurl, :postalcode, :natureofbusiness, :companyaddress, :industry)';
+			
+			echo "<b>SQL: </b>".$sql."<br><br>";
+			$stid = oci_parse($dbh, $sql);
+
+			oci_bind_by_name($stid, ":email", $email);
+			oci_bind_by_name($stid, ":password", $password);
+			oci_bind_by_name($stid, ":companyname", $companyname);
+			oci_bind_by_name($stid, ":companynum", $companynum);
+			oci_bind_by_name($stid, ":companyurl", $companyurl);
+			oci_bind_by_name($stid, ":postalcode", $postalcode);
+			oci_bind_by_name($stid, ":natureofbusiness", $natureofbusiness);
+			oci_bind_by_name($stid, ":companyaddress", $companyaddress);
+			oci_bind_by_name($stid, ":industry", $industry);
+
+	
+			oci_execute($stid);
+			while($row = oci_fetch_array($stid)) {
+			echo "<tr>";
+			echo "<td>" . $row[0] . "</td>";
+			echo "<td>" . $row[1] . "</td>";
+			echo "</tr>";
+			}
+			oci_free_statement($stid);
+			}
+		?>
+		
+		</div>
+    </div>
+ </div>
+</div>
+<!-- footer --> 
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 grid_3">
+			<h4>Navigate</h4>
+			<ul class="f_list f_list1">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="applicantLogin.html">Sign In</a></li>
+				<li><a href="applicantRegister.html">Join Now</a></li>
+				<li><a href="about.html">About</a></li>
+			</ul>
+			<ul class="f_list">
+				<li><a href ="jobs.html">Find a Job</a></li>
+				<li><a href="contact.html">Contact Us</a></li>
+				<li><a href="post.html">Post a Job</a></li>
+			</ul>
+			<div class="clearfix"> </div>
+		</div>
+		<div class ="col-md-4 grid 3">
+		</div>
+		<div class="col-md-4 grid_3">
+			<h4>Sign up for our newsletter</h4>
+			<p>Enter your email below and we will send updates into your inbox.</p>
+			<form>
+				<input type="text" class="form-control" placeholder="Enter your email">
+				<button type="button" class="btn red">Subscribe now!</button>
+		    </form>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+<div class="footer_bottom">	
+  <div class="container">
+	<div class="copy">
+		<p>This is a CS2102 Project.</a> </p>
+	</div>
+  </div>
+</div>
+<?php
+oci_close($dbh);
+?>
+</body>
+</html>	
