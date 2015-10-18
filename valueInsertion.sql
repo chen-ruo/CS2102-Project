309b25aa42f99558e1934f6a7a38b895ea45b231
Chen Ruo <chen.ruo@hotmail.com>
2015-10-18 18:31:05 +0800

    Insert value into database: applicant, employer, job
    A few data to be inserted into database for testing.

---
+++ valueInsertion.sql
@@ -0,0 +1,11 @@
+/*Applicants*/
+INSERT INTO applicant VALUES('Ruo','Chen','chen.ruo@hotmail.com','helloworld','23','90042203','Female','14-APR-92','Chinese',NULL);
+INSERT INTO applicant VALUES('RenFei','Wang','wangrenfei1991@gmail.com','applepie','24','91032301','Male','11-SEP-91','Chinese',NULL);
+
+/*Employer*/
+INSERT INTO employer VALUES('a0099726@u.nus.edu','12345','National University of Singapore','60000000',NULL,000000,'University','Lower Kent Ridge','Education');
+INSERT INTO employer VALUES('a0101002@u.nus.edu','54321','Bank of China','61000000',NULL,000001,'Finance','4 Battery Road','Banking');
+
+
+/*Job*/
+INSERT INTO job VALUES('1','Permanent','a0099726@u.nus.edu','IT','Degree','C++',NULL);
