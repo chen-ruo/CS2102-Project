30886b165a49ae5b8f8cb3e591972f67265a7662
Chen Ruo <chen.ruo@hotmail.com>
2015-10-18 16:49:19 +0800

    Index & about us page.
    Index page is the main page that eveyone will see. About is the
    background of the portal and team involved in the development. Both are
    untouched because no command is required at the moment.

---
+++ about.php
@@ -0,0 +1,286 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="icon" href="http://i376.photobucket.com/albums/oo207/happyice/favicon-32x32_zpsci38sgpc.png">
+<title>One stop Job Portal</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="One stop job portal" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""/></a>
+	    </div>
+	    <!--/.navbar-header-->
+	    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
+	        <ul class="nav navbar-nav">
+			   <li> <a href="#testimonials"> Our Testimonials and team</a></li>
+		        <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jobs<b class="caret"></b></a>
+		            <ul class="dropdown-menu">
+			            <li><a href="jobs.html">Part-time Jobs</a></li>
+			            <li><a href="jobs.html">Internships</a></li>
+			            <li><a href="jobs.html">Full-time Jobs</a></li>
+		            </ul>
+		        </li>
+				   <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Employers<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="post.html">Post Jobs</a></li>
+						    <li><a href="search.html">Search applicants</a></li>
+							  <li><a href="searchmatched.html">Search for matched applicants</a></li>
+		             </ul>
+		        </li>
+				
+				<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerLogin.html">Employer login</a></li>
+						    <li><a href="applicantLogin.html">Applicant Login</a></li>
+		             </ul>
+		        </li>
+			<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Registration<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerRegister.html">Create an employer account</a></li>
+						    <li><a href="applicantRegister.html">Create an applicant account</a></li>
+		             </ul>
+		        </li>
+	    </div>
+	    <div class="clearfix"> </div>
+	  </div>
+	    <!--/.navbar-collapse-->
+	</nav>
+<div class="banner_1">
+	<div class="container">
+		<div id="search_wrapper">
+		 <div id="search_form" class="clearfix">
+		 <h1>Start your job search</h1>
+		    <p>
+			 <input type="text" class="text" placeholder=" " value="Enter Keyword(s)" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Keyword(s)';}">
+			 <input type="text" class="text" placeholder=" " value="Location" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Location';}">
+			 <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Find Jobs"></label>
+			</p>
+			</div>
+			</div>
+			</div>
+			</div>
+			
+			
+<!-- End of header -->
+
+<div class="container">
+    <div class="single">  
+       <h2>Advantages</h2>
+       <div class="about_top">
+	     <div class="col-md-4 box_7">
+  			<div class="list styled custom-list custom-list1">
+				<ul>
+					<li><span class="dropcap">1.</span>  
+					   <div class="item_content">
+						<h5>Job Searching</h5> 
+						<p>Our Portal comprises of companies from any industries.</p>
+					   </div>
+					</li>
+			    </ul>
+			</div>
+	     </div>
+	     <div class="col-md-4 box_7">
+  			<div class="list styled custom-list custom-list1">
+				<ul>
+					<li><span class="dropcap">2.</span>  
+					   <div class="item_content">
+						<h5>Job Application</h5> 
+						<p>Login with an account and job application is just a click away.</p>
+
+					   </div>
+					</li>
+			    </ul>
+			</div>
+	     </div>
+	     <div class="col-md-4 box_7">
+  			<div class="list styled custom-list custom-list1">
+				<ul>
+					<li><span class="dropcap">3.</span>  
+					   <div class="item_content">
+						<h5>Job Matching</h5> 
+						<p>Jobs will be automatically matched by our smart system. Once matched, employer will be informed of the applicants matched.</p>
+					   </div>
+					</li>
+			    </ul>
+			</div>
+	     </div>
+	     <div class="clearfix"> </div>
+	   </div>
+	</div>
+</div>
+<div class="about_middle">
+	<div class="container" id = "testimonials">
+         <div class="wmuSlider example1">
+			   <div class="wmuSliderWrapper">
+			   	   <h3>Testimonials</h3>
+				   <article style="position: absolute; width: 100%; opacity: 0;"> 
+				   	    <div class="banner-wrap">
+				   	 	    <ul class="grid-1">
+				   	 			<li class="grid-1_left">
+				   	 				<img src="images/f5.jpg" class="img-responsive" alt=""/>
+				   	 			</li>
+				   	 			<li class="grid-1_right">
+				   	 				<p>Job Portal is a great platform for me to advertise my job online! Great!</p>
+								      <h4><a href="#">Alicia</a> | Accenture HR</h4>
+				   	 			</li>
+				   	 			<div class="clearfix"> </div>
+				   	 		</ul>
+				   	    </div>
+					</article>
+				   <article style="position: absolute; width: 100%; opacity: 0;"> 
+				   	    <div class="banner-wrap">
+				   	 	    <ul class="grid-1">
+				   	 			<li class="grid-1_left">
+				   	 				<img src="images/f6.jpg" class="img-responsive" alt=""/>
+				   	 			</li>
+				   	 			<li class="grid-1_right">
+				   	 				<p>Great tool for posting job offers!</p>
+								      <h4><a href="#">John doe</a> | Exonmobil</h4>
+				   	 			</li>
+				   	 			<div class="clearfix"> </div>
+				   	 		</ul>
+				   	    </div>
+					</article>
+				   <article style="position: absolute; width: 100%; opacity: 0;"> 
+				   	    <div class="banner-wrap">
+				   	 	    <ul class="grid-1">
+				   	 			<li class="grid-1_left">
+				   	 				<img src="images/f7.jpg" class="img-responsive" alt=""/>
+				   	 			</li>
+				   	 			<li class="grid-1_right">
+				   	 				<p>Received a job offer through this portal. Nice job portal!</p>
+								      <h4><a href="#">annette Doe</a> | Applicant</h4>
+				   	 			</li>
+				   	 			<div class="clearfix"> </div>
+				   	 		</ul>
+				   	    </div>
+					</article>
+				 </div>
+				<ul class="wmuSliderPagination">
+                	<li><a href="#" class="">0</a></li>
+                	<li><a href="#" class="">1</a></li>
+                	<li><a href="#" class="wmuActive">2</a></li>
+                </ul>
+            </div>
+            <script src="js/jquery.wmuSlider.js"></script> 
+			  <script>
+       			$('.example1').wmuSlider();         
+   		     </script> 	           	      
+      </div>
+</div>
+<div class="about_bottom">
+	<div class="container">
+		<h3>Our Team</h3>
+	    <div class="col-md-3 team-member text-center">
+			<h5>Nicholas</h5>
+			<h6></h6>
+			<p>Having fun programming</p>
+			<a href="#"><i class="fa fa-facebook fa1"> </i></a>
+			<a href="#"><i class="fa fa-twitter fa1"> </i></a>
+			<a href="#"><i class="fa fa-linkedin fa1"> </i></a>
+			<a href="#"><i class="fa fa-instagram fa1"> </i></a>
+		</div>
+		<div class="col-md-3 team-member text-center">
+			<h5>Chen ruo</h5>
+			<h6></h6>
+			<p>Having fun programming</p>
+			<a href="#"><i class="fa fa-facebook fa1"> </i></a>
+			<a href="#"><i class="fa fa-twitter fa1"> </i></a>
+			<a href="#"><i class="fa fa-linkedin fa1"> </i></a>
+			<a href="#"><i class="fa fa-instagram fa1"> </i></a>
+		</div>
+		<div class="col-md-3 team-member text-center">
+			<h5>Ren fei</h5>
+			<h6></h6>
+			<p>Having fun programming</p>
+			<a href="#"><i class="fa fa-facebook fa1"> </i></a>
+			<a href="#"><i class="fa fa-twitter fa1"> </i></a>
+			<a href="#"><i class="fa fa-linkedin fa1"> </i></a>
+			<a href="#"><i class="fa fa-instagram fa1"> </i></a>
+		</div>
+		<div class="col-md-3 team-member text-center">
+			<h5>Jeshua</h5>
+			<h6></h6>
+			<p>Having fun programming</p>
+			<a href="#"><i class="fa fa-facebook fa1"> </i></a>
+			<a href="#"><i class="fa fa-twitter fa1"> </i></a>
+			<a href="#"><i class="fa fa-linkedin fa1"> </i></a>
+			<a href="#"><i class="fa fa-instagram fa1"> </i></a>
+		</div>
+		  <div class="col-md-3 team-member text-center">
+			<h5>Ruo xi</h5>
+			<h6></h6>
+			<p>Having fun programming</p>
+			<a href="#"><i class="fa fa-facebook fa1"> </i></a>
+			<a href="#"><i class="fa fa-twitter fa1"> </i></a>
+			<a href="#"><i class="fa fa-linkedin fa1"> </i></a>
+			<a href="#"><i class="fa fa-instagram fa1"> </i></a>
+		</div>
+		<div class="clearfix"> </div>
+	</div>	
+</div>
+<!-- footer --> 
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 grid_3">
+			<h4>Navigate</h4>
+			<ul class="f_list f_list1">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="applicantLogin.html">Sign In</a></li>
+				<li><a href="applicantRegister.html">Join Now</a></li>
+				<li><a href="about.html">About</a></li>
+			</ul>
+			<ul class="f_list">
+				<li><a href ="jobs.html">Find a Job</a></li>
+				<li><a href="contact.html">Contact Us</a></li>
+				<li><a href="post.html">Post a Job</a></li>
+			</ul>
+			<div class="clearfix"> </div>
+		</div>
+		<div class ="col-md-4 grid 3">
+		</div>
+		<div class="col-md-4 grid_3">
+			<h4>Sign up for our newsletter</h4>
+			<p>Enter your email below and we will send updates into your inbox.</p>
+			<form>
+				<input type="text" class="form-control" placeholder="Enter your email">
+				<button type="button" class="btn red">Subscribe now!</button>
+		    </form>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+<div class="footer_bottom">	
+  <div class="container">
+	<div class="copy">
+		<p>This is a CS2102 Project.</a> </p>
+	</div>
+  </div>
+</div>
+</body>
+</html>	
---
+++ index.php
@@ -0,0 +1,250 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="icon" href="http://i376.photobucket.com/albums/oo207/happyice/favicon-32x32_zpsci38sgpc.png">
+<title>One stop Job Portal</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="One stop job portal" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""/></a>
+	    </div>
+	    <!--/.navbar-header-->
+	    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
+	        <ul class="nav navbar-nav">
+			  <li><a href="about.html">About Us</a></li>
+			   <li> <a href="#process">Our process</a></li>
+				<li><a href="contact.html">contact Us</a></li>
+		        <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jobs<b class="caret"></b></a>
+		            <ul class="dropdown-menu">
+			            <li><a href="jobs.html">Part-time Jobs</a></li>
+			            <li><a href="jobs.html">Internships</a></li>
+			            <li><a href="jobs.html">Full-time Jobs</a></li>
+		            </ul>
+		        </li>
+				        <li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Employers<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="post.html">Post Jobs</a></li>
+						    <li><a href="search.html">Search applicants</a></li>
+							  <li><a href="searchmatched.html">Search for matched applicants</a></li>
+		             </ul>
+		        </li>
+				
+				<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerLogin.html">Employer login</a></li>
+						    <li><a href="applicantLogin.html">Applicant Login</a></li>
+		             </ul>
+		        </li>
+			<li class="dropdown">
+		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Registration<b class="caret"></b></a>
+		             <ul class="dropdown-menu">
+						  <li><a href="employerRegister.html">Create an employer account</a></li>
+						    <li><a href="applicantRegister.html">Create an applicant account</a></li>
+		             </ul>
+		        </li>
+	    </div>
+	    <div class="clearfix"> </div>
+	  </div>
+	    <!--/.navbar-collapse-->
+	</nav>
+<div class="banner">
+	<div class="container">
+		<div id="search_wrapper">
+		 <div id="search_form" class="clearfix">
+		 <h1>Start your job search</h1>
+		    <p>
+			 <input type="text" class="text" placeholder=" " value="Enter Keyword(s)" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Keyword(s)';}">
+			 <input type="text" class="text" placeholder=" " value="Location" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Location';}">
+			 <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Find Jobs"></label>
+			</p>
+            <h2 class="title">Top searches</h2>
+         </div>
+		 <div id="city_1" class="clearfix">
+			 <ul class="orange">
+			 </li>
+			 <li>
+			 <a href="#" title="Fresher">Fresher</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Engineering">Engineering</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Finance Jobs">Finance</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Teaching">Teaching</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Software">Software</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Accounting">Accounting</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Data Entry">Data Entry</a>
+			 </li>
+			 </ul>
+			 <ul class="blue">
+			 <li>
+			 <a href="#" title="Marketing Jobs">Marketing </a>
+			 </li>
+			 <li>
+			 <a href="#" title="Freelancer">Freelancer</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Internet Jobs">Internet</a>
+			 </li>
+			 <li>
+			 <a href="#" title="Sales">Sales</a>
+			 </li>
+			  <li>
+			 <a href="#" title="Legal">Legal</a>
+			 </li>
+			 <li>
+			 <a href="#" title="HR">HR</a>
+			 </li>
+			 <li>
+			 </li>
+			 </ul>
+	     </div>
+       </div>
+   </div> 
+</div>	
+<div class="container">
+  <div class="grid_1">
+	 <h3>Featured Employers</h3>
+	   <ul id="flexiselDemo3">
+	      <li><img src="images/c1.gif"  class="img-responsive" /></li>
+		  <li><img src="images/c2.gif"  class="img-responsive" /></li>
+		  <li><img src="images/c3.gif"  class="img-responsive" /></li>
+		  <li><img src="images/c4.gif"  class="img-responsive" /></li>
+		  <li><img src="images/c5.gif"  class="img-responsive" /></li>
+		  <li><img src="images/c6.gif"  class="img-responsive" /></li>	
+	    </ul>
+	    <script type="text/javascript">
+		 $(window).load(function() {
+			$("#flexiselDemo3").flexisel({
+				visibleItems: 6,
+				animationSpeed: 1000,
+				autoPlay:false,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	   </script>
+	   <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+	   <div class="clearfix"></div>
+	 </div>
+	 <!--Our process -->
+	 	<div class="container" id = "process">
+		  <div class="grid_1">
+				<h3>Our Process</h3>
+		</div>
+		</div>
+		<div class="our">
+			<div class="our-top">
+				<div class="col-md-4 our-left">
+					<i class="best"></i>
+					<div><img src="images/works1.png"/></div>
+					<h3>Searching for the best job</h3>	
+					<p>Search a wide variety of jobs with our portal.</p>
+				</div>
+				<div class="col-md-4 our-left">
+					<i class="resu"></i>
+					<div><img src="images/works2.png"/></div>
+					<h3>Apply for the job</h3>	
+					<p>Apply for a job with convenience.</p>
+				</div>
+				<div class="col-md-4 our-left">
+					<i class="interv"></i>
+					<div><img src="images/works3.png"/></div>
+					<h3>Route to getting employed</h3>	
+					<p>Once applied, employer will be informed of the application.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+<br><br><br><br>
+<!-- footer --> 
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 grid_3">
+			<h4>Navigate</h4>
+			<ul class="f_list f_list1">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="applicantLogin.html">Sign In</a></li>
+				<li><a href="applicantRegister.html">Join Now</a></li>
+				<li><a href="about.html">About</a></li>
+			</ul>
+			<ul class="f_list">
+				<li><a href ="jobs.html">Find a Job</a></li>
+				<li><a href="contact.html">Contact Us</a></li>
+				<li><a href="post.html">Post a Job</a></li>
+			</ul>
+			<div class="clearfix"> </div>
+		</div>
+		<div class ="col-md-4 grid 3">
+		</div>
+		<div class="col-md-4 grid_3">
+			<h4>Sign up for our newsletter</h4>
+			<p>Enter your email below and we will send updates into your inbox.</p>
+			<form>
+				<input type="text" class="form-control" placeholder="Enter your email">
+				<button type="button" class="btn red">Subscribe now!</button>
+		    </form>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+<div class="footer_bottom">	
+  <div class="container">
+	<div class="copy">
+		<p>This is a CS2102 Project.</a> </p>
+	</div>
+  </div>
+</div>
+</body>
+</html>	
