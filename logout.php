d282a41312f07f56f86e0d7904b41b258e13f07f
Chen Ruo <chen.ruo@hotmail.com>
2015-11-01 02:34:14 +0800

    modified logout to have alert box and redirect to index

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 session_destroy();
-echo 'You have been logged out. <a href="index.php">Go back</a>';
-
+echo'<script>';
+echo'alert("You have been logged out.");';
+echo 'window.location.href = "index.php";';
+echo '</script>';
 ?>
